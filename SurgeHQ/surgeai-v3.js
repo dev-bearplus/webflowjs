c02643031b3f6672fdb8d774a16523cfdf8f5068
nqnduy <nqn.duy@gmail.com>
2025-10-01 18:21:51 +0700

    surgeai: v3

--- SurgeHQ/surgeai-v3.js
+++ SurgeHQ/surgeai-v3.js
@@ -27,41 +27,6 @@ const script = () => {
 
     SCRIPT.homeScript = () => {
         console.log("home");
-        $('.home-search-marquee-list').each((idx, el) => {
-            marquee($(el));
-            idx % 2 === 1 && $(el).css('animation-direction', 'reverse');
-        })
-        $('.home-faq-list-wrap').each((idx, el) => {
-            let parent = childSelect(el);
-
-            parent('.home-faq-item').on('click', function (e) {
-                $(this).toggleClass('active');
-                parent('.home-faq-item').not(this).removeClass('active');
-            })
-        })
-
-        if ($(window).width() > 767) {
-            $('.home-search-answer-stick').css('top', ($(window).height() - $('.home-search-answer-stick').height()) / 2);
-        }
-        else {
-            requestAnimationFrame(() => {
-                $('.home-search-answer-list-wrap').css('display', 'block');
-                requestAnimationFrame(() => {
-                    $('.home-search-answer-list-wrap').css({
-                        '--grid-row-height': `${$('.home-search-answer-list-inner').height()}px`,
-                        'display': 'grid'
-                    });
-                    requestAnimationFrame(() => {
-                        $('.home-search-answer-list-wrap').css({
-                            'grid-template-rows': `${$('.home-search-answer-list').height()}px`,
-                        });
-                    })
-                })
-            })
-            $('.home-search-answer-more-btn').on('click', function (e) {
-                $('.home-search-answer-list-wrap').addClass('active');
-            })
-        }
     }
 
     scrollTop();
