17418c9219b1ff77a5cd394c5163bf8143457355
nqnduy <nqn.duy@gmail.com>
2025-10-02 12:36:41 +0700

    surgeai: v3

--- SurgeHQ/surgeai-v3.js
+++ SurgeHQ/surgeai-v3.js
@@ -29,6 +29,43 @@ const script = () => {
         console.log("home");
     }
 
+    SCRIPT.postScript = () => {
+        if ($(window).width() > 991) {
+            let headings = $('.post-content-richtext h2');
+            let tocWrap = $('.post-content-toc-list');
+
+            let tocClone = $('.post-content-toc-item').eq(0).clone();
+            tocWrap.html('');
+            headings.each(function (idx, heading) {
+                let text = $(heading).text().replace(/^\d+\.\s*/, '').replace(/\s*\([^)]*\)/g, '');
+                let id = text.toLowerCase().trim().replace(/[\s\W-]+/g, '-').replace(/^-+|-+$/g, '');
+                let link = tocClone.clone();
+
+                $(heading).attr('id', id);
+                link.attr('href', `#${id}`);
+                link.find('.txt').text(text);
+                idx === 0 && link.addClass('w--current');
+
+                tocWrap.append(link);
+            })
+            $(window).on('scroll', function (e) {
+                const scrollTop = document.documentElement.scrollTop || window.scrollY
+                headings.each((idx, heading) => {
+                    if (scrollTop > $(heading).offset().top - ($(window).height() * .5)) {
+                        $(`.post-content-toc-item[href="#${$(heading).attr('id')}"]`).addClass('w--current');
+                        $(`.post-content-toc-item[href="#${$(heading).attr('id')}"]`).siblings().removeClass('w--current');
+                    }
+                })
+            })
+
+            const currToc = window.location.hash;
+            if ($(currToc).length) {
+                setTimeout(() => $(`.post-content-toc-item[href="${currToc}"]`).trigger('click'), 400);
+                setTimeout(() => $(`.post-content-toc-item[href="${currToc}"]`).trigger('click'), 800);
+            }
+        }
+    }
+
     scrollTop();
 
     const pageName = $('main.main').attr('data-namespace');
