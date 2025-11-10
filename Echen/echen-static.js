7906a7bff522b3feed5c949a92f05dee63878f80
nqnduy <nqn.duy@gmail.com>
2025-11-10 17:28:04 +0700

    update animation

--- Echen/echen-static.js
+++ Echen/echen-static.js
@@ -224,7 +224,6 @@ const mainScript = () => {
                 });
 
                 smoothScroll.lenis.on('scroll', (e) => {
-                    // gsap.set($(this.el).find('.note-content-links-totop'), { '--progress': `${e.progress * 100}%` })
                     if (viewport.w <= 767) {
                         gsap.set($(this.el).find('.note-header-cms-item-link.w--current').siblings('.line').find('.line-inner'), { scaleX: e.progress })
                     }
