f1ca3df15cfa08efe250e414e28f953bbd69aa24
nqnduy <nqn.duy@gmail.com>
2025-11-15 10:18:28 +0700

    update animation

--- noah/nellc.js
+++ noah/nellc.js
@@ -295,11 +295,11 @@ const script = () => {
                     .fromTo('.home-hero-front-cta', { autoAlpha: 1, y: 0 }, { autoAlpha: 0, y: 10, duration: .6, ease: 'none' }, 0)
                     .to('.home-hero-front-sub-img.left', { xPercent: -20, yPercent: -20, autoAlpha: 0, duration: .6, ease: 'none' }, 0)
                     .to('.home-hero-front-sub-img.right', { xPercent: 20, yPercent: -20, autoAlpha: 0, duration: .6, ease: 'none' }, 0)
-                    .to('.home-hero-front-title-flex:nth-child(3) .heading:nth-child(1)', { x: cvUnit(-250, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'expo.inOut' }, "<=.1")
-                    .to('.home-hero-front-title-flex:nth-child(3) .heading:nth-child(2)', { x: cvUnit(285, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'expo.inOut' }, '<=0')
-                    .to('.home-hero-front-title-flex:nth-child(2) .heading:nth-child(1)', { x: cvUnit(-340, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'expo.inOut' }, "<=.3")
-                    .to('.home-hero-front-title-flex:nth-child(2) .heading:nth-child(2)', { x: cvUnit(250, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'expo.inOut' }, '<=0')
-                    .to('.home-hero-front-title .heading:nth-child(1)', { y: -cvUnit(10, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'power1.inOut' }, '<=0.1')
+                    .to('.home-hero-front-title-flex:nth-child(3) .heading:nth-child(1)', { x: cvUnit(-250, 'rem'), y: -cvUnit(15, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'power2.inOut' }, "<=.1")
+                    .to('.home-hero-front-title-flex:nth-child(3) .heading:nth-child(2)', { x: cvUnit(285, 'rem'), y: -cvUnit(15, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'power2.inOut' }, '<=0')
+                    .to('.home-hero-front-title-flex:nth-child(2) .heading:nth-child(1)', { x: cvUnit(-340, 'rem'), y: -cvUnit(15, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'power2.inOut' }, "<=.3")
+                    .to('.home-hero-front-title-flex:nth-child(2) .heading:nth-child(2)', { x: cvUnit(250, 'rem'), y: -cvUnit(15, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'power2.inOut' }, '<=0')
+                    .to('.home-hero-front-title .heading:nth-child(1)', { y: -cvUnit(15, 'rem'), scale: .9, autoAlpha: 0, duration: .6, ease: 'power2.inOut' }, '<=0.1')
                     .fromTo('.home-hero-front-main-img',
                         { scale: 1, y: -middleOffsetTop, width: originalWidth, transformOrigin: `center bottom` },
                         { scale: scaleOffSet, y: offsetBottom, width: originalWidth * scaleOffSet, duration: 1,  ease: 'power1.inOut'
@@ -307,7 +307,7 @@ const script = () => {
                     .to('.home-hero-front-main-img-inner', { marginLeft: 0, duration: .7, ease: 'power1.inOut'  }, "<=0")
                     .to('.home-hero-front-main-img-bg', { borderRadius: 0, borderWidth: 0, duration: .8,  ease: 'power1.inOut' }, "<=0")
                     .to('.home-hero-front', { autoAlpha: 0, duration: .8,  ease: 'power1.inOut'  }, "-=.25")
-                    .to('.home-hero-text-wrap', { y: 0, duration: 1, ease: 'power1.inOut' }, "<=0")
+                    .to('.home-hero-text-wrap', { y: 0, duration: 1, ease: 'power1.inOut' }, "<=0.1")
             }
             interact() {
             }
