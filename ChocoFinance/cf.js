b9e90d8f98245b438703d762721774754ea72639
kelvin0601 <kelvin@bear.plus>
2025-12-30 17:23:44 +0700

    update choco

--- ChocoFinance/cf.js
+++ ChocoFinance/cf.js
@@ -67,6 +67,23 @@ const mainScript = () => {
             timer = setTimeout(func, delay, event);
         };
     }
+    function initChatbox() {
+        let lang = $('html').attr('lang');
+        let key ;
+        if(lang == 'en-HK' || lang == 'zh-HK') {
+            key ='39299081-8fb6-4a91-9e77-2feea792353b'
+        } 
+        else {
+            key ='828c3b01-7155-4ead-9114-3d244832fa64'
+        }
+        $('<script/>', {
+            id: 'ze-snippet',
+            src: `https://static.zdassets.com/ekr/snippet.js?key=${key}`,
+            async: true
+        }).appendTo('body');
+        console.log('chatbox initialized');
+    }
+    initChatbox();
     function isInViewport(el) {
         const rect = el.getBoundingClientRect();
         return (
