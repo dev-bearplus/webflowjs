666a6aa6457f971bc80475988855b86ec5d8187f
kelvin0601 <kelvin@bear.plus>
2025-12-30 17:26:56 +0700

    update choco

--- ChocoFinance/cf.js
+++ ChocoFinance/cf.js
@@ -71,7 +71,7 @@ const mainScript = () => {
         let lang = $('html').attr('lang');
         let key ;
         if(lang == 'en-HK' || lang == 'zh-HK') {
-            key ='39299081-8fb6-4a91-9e77-2feea792353b'
+            key ='828c3b01-7155-4ead-9114-3d244832fa64'
         } 
         else {
             key ='828c3b01-7155-4ead-9114-3d244832fa64'
